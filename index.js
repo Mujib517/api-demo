6e26978d0c00bebc9bf1db181e1f45fb116056fc
Mujib <mujib517@gmail.com>
2021-08-15 11:36:04 +0530

    Add morgan request logging

--- index.js
+++ index.js
@@ -3,7 +3,10 @@
 // ES6 module (import, export)
 // middleware
 const express = require('express');
+const path = require('path');
+const fs = require('fs');
 const bodyParser = require('body-parser');
+const morgan = require('morgan');
 const booksRouter = require('./routes/bookRouter');
 const defaultRouter = require('./routes/defaultRouter');
 const app = express();
@@ -12,10 +15,14 @@ app.listen(3000, function () {
   console.log("Server is running on 3000");
 });
 
+var accessLogStream = fs.createWriteStream(path.join(__dirname, 'logs', 'requests.log'), { flags: 'a' })
+
 const removePoweredByHeader = (req, res, next) => {
   res.setHeader('X-Powered-By', '');
   next();
 }
+
+app.use(morgan('combined', { stream: accessLogStream }));
 
 // middlewares
 app.use(removePoweredByHeader);
