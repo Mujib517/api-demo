86c48db9677a15b0dc9ed3e4608d2757ceb2836e
Mujib <mujib517@gmail.com>
2021-09-06 18:21:04 +0530

    change ssl settings for db con

--- config/config.js
+++ config/config.js
@@ -17,7 +17,10 @@ module.exports = {
         "use_env_variable": "DATABASE_URL",
         "dialect": "postgres",
         "dialectOptions": {
-            "ssl": true
+            "ssl": {
+                "require":true,
+                "rejectUnauthorized": false,
+            }
         }
     }
 }
